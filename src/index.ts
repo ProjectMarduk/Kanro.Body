bda885d9d2f36da9da0e9d5ff34d51c56314917c
HIGAN <higan@live.cn>
2017-03-24 23:58:39 +0800

    Add declare for IRequest

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,46 @@
 import * as BodyParserCore from "body-parser";
+import * as MulterCore from "multer";
 import { Kanro } from "kanro.core";
+
+declare module "kanro.core" {
+    namespace Kanro {
+        namespace Core {
+            interface IRequest {
+                body: { [name: string]: any };
+                file: Express.Multer.File;
+                files: {
+                    [fieldName: string]: Express.Multer.File[];
+                };
+            }
+        }
+    }
+}
 
 export namespace Body {
     export class BodyParser extends Kanro.BaseRequestHandler {
         async handler(request: Kanro.Core.IRequest): Promise<Kanro.Core.IRequest> {
             await new Promise((res, rej) => {
                 BodyParserCore.json()(<any>request.meta, undefined, (err) => {
-                    if(err){
+                    if (err) {
                         rej(err);
                         return;
                     }
-                    request["body"] = request.meta["body"];
+                    request.body = request.meta["body"];
                     res();
                 })
             });
             await new Promise((res, rej) => {
-                BodyParserCore.urlencoded({extended : true})(<any>request.meta, undefined, (err) => {
-                    if(err){
+                BodyParserCore.urlencoded({ extended: true })(<any>request.meta, undefined, (err) => {
+                    if (err) {
                         rej(err);
                         return;
                     }
-                    request["body"] = request.meta["body"];
+                    request.body = request.meta["body"];
                     res();
                 })
             });
+
+            MulterCore({dest : "./resource/upload"})
             return request;
         }
 
